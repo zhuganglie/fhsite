0835cf594594dfc9d810f5dbad782b36ec381984
pyrrho <pyrrhonianpig@gmail.com>
2024-08-07 17:10:32 +0800

    update

--- components.py
+++ components.py
@@ -86,10 +86,10 @@ def blog_footer():
     )
 
 def tag(slug: str):
-    return A(slug, href=f"/tags/{urllib.parse.unquote(slug)}")
+    return A(slug, href=f"/tags/{urllib.parse.quote(slug)}")
 
 def tag_with_count(slug: str, count: int):
-    return A(Span(slug), Small(f"({count})"), href=f"/tags/{urllib.parse.unquote(slug)}")
+    return A(Span(slug), Small(f"({count})"), href=f"/tags/{urllib.parse.quote(slug)}")
 
 def markdown_page(slug: str):
     try:
